d17ef98312f1de4ad401050b7ffa746a09f8fad9
SubhamReddddy <subhamreddy121@gmail.com>
2026-02-28 21:07:09 +0530

    added dbConnection to env

--- Config/database.js
+++ Config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const DB_Connection = () => {
   mongoose
     .connect(
-      "mongodb+srv://subhamreddy121:lctJTPLGhWkKC0Dh@cluster0.1qyxmdh.mongodb.net/Amazon_clone"
+      process.env.DBConnection,
     )
     .then(() => {
       console.log("successfully connected to mongodb database!");
